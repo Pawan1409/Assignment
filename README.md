c06543ac0ab001b5ce7005fbbcd9e775d3ad6335
Pawan Parihar <pawan.14jics064@jietjodhpur.ac.in>
2018-10-19 01:02:38 +0530

    Navigation Bar menu generated using data from JSON file.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment
+
+Project made using HTML, CSS and JavaScript
+
+Used SmartMenu jQuery plugin for generating dynamic menu in Navigation Bar using data from JSON File.
+
+Fetched categories and subcategories from JSON file kept in assets folder. 
---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#filters").hide();
+    $("#products").hide();
+
+
+    var arrCategories;
+    $.getJSON("/assets/data.json", function (data) {
+        arrCategories = data;
+        $('#mainMenu').renderizeMenu(arrCategories, { rootClass: "nav navbar-nav mr-auto", ulParentClass: "dropdown-menu", dropdownIcon: '<span class="caret"></span>' });
+        jQuery.SmartMenus.Bootstrap.init();
+    })
+    showSlides();
+})
+
+// When the user scrolls the page, execute Function
+window.onscroll = function() {fixHeader()};
+
+// Get the header
+var header = document.getElementById("mainNavbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function fixHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    }
+    else {
+        header.classList.remove("sticky");
+    }
+}
+
+//Slideshow
+var slideIndex=0;
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
